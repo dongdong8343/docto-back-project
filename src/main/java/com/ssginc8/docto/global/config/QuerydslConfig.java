5cc7f89bbb3ebea0e4a91d2ee6ad85dbaefc6015
dongdong8343 <fkdlem8343@naver.com>
2025-07-24 17:33:45 +0900

    Rename: QuerydslConfig 파일 global 패키지로 이동

--- src/main/java/com/ssginc8/docto/global/config/QuerydslConfig.java
+++ src/main/java/com/ssginc8/docto/global/config/QuerydslConfig.java
@@ -1,4 +1,4 @@
-package com.ssginc8.docto.file.config;
+package com.ssginc8.docto.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
